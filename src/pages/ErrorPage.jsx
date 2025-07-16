1c8a7e5f7b3b7c16e0a1d585610cef9bc8f04d8b
mottasimsadi <mottasimsadi@gmail.com>
2025-07-17 02:09:11 +0600

    Completed the error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,38 @@
-import { useRouteError } from "react-router";
+import { Link, useLocation } from "react-router";
+import animation from "../assets/PageNotFound404.json";
+import Lottie from "lottie-react";
+import { useEffect } from "react";
 
 const ErrorPage = () => {
-  const error = useRouteError();
-  console.log(error);
+  const location = useLocation();
+
+  useEffect(() => {
+      console.error("404 Error: Page not found →", location.pathname);
+    }, [location.pathname]);
+  
   return (
-    <div>
-      <div className="flex min-h-screen items-center justify-center">Error</div>
+    <div className="bg-white min-h-screen flex flex-col justify-center items-center px-4 py-8">
+      <div className="max-w-3xl">
+        <Lottie animationData={animation} loop={true} />
+      </div>
+
+      <div className="text-center space-y-8 max-w-2xl">
+        <p className="text-lg leading-relaxed text-base-100">
+          The page at
+          <code className="px-2 py-1 rounded mx-1 text-[#ff6b35]">
+            {location.pathname}
+          </code>
+          doesn't exist or has been moved.
+        </p>
+
+        <div className="pt-4">
+          <Link to="/" aria-label="Return to home page">
+            <button className="px-8 py-3 rounded-lg font-semibold text-lg shadow-md text-[#ff6b35] border cursor-pointer transition-all duration-300 hover:shadow-xl hover:-translate-y-0.5 hover:bg-[#ff6b35] hover:text-white">
+              ← Return to Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
